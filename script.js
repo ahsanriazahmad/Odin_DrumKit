0ada8275387a57f9388bc864be8e8ff31fd5e596
ahsanriazahmad <ahsanriazahmad1@gmail.com>
2022-02-27 14:35:03 -0800

    updated id back to data-key

--- script.js
+++ script.js
@@ -21,7 +21,7 @@
 
 function playSound() {
   //console.log(e);
-  const audio = document.querySelector(`audio[id= 65]`);
+  const audio = document.querySelector(`audio[data-key = 65]`);
   if (!audio) return; //stop the function from running all together
   audio.currentTime = 0; // rewind to the start of the audio
   audio.play();
